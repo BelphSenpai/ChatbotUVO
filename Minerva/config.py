2f9eea00f35e97818a9af0836b0a967f4608c9b7
Nil Lopez <nilolo@jviladoms.cat>
2025-03-22 12:59:22 +0100

    Minerva subdividida en 5 para 5 personalidades y datasheets

---
+++ Minerva/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables del .env
+load_dotenv()
+
+# Claves API
+OPENAI_API_KEY = os.getenv("API_KEY")
+DISCORD_TOKEN = os.getenv("TOKEN_BOT")
