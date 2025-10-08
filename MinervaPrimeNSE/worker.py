bf356290dc1c84f0fd372fae01347f0db041719c
Nil López <nil.lopez@telma.ai>
2025-10-08 10:32:37 +0200

    fixes

--- MinervaPrimeNSE/worker.py
+++ MinervaPrimeNSE/worker.py
@@ -1,12 +1,14 @@
+# MinervaPrimeNSE/worker.py
 import os
 from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
-listen = ['queries']
+listen = os.getenv('RQ_QUEUES', 'queries').split(',')
 redis_url = os.getenv('REDIS_URL', 'redis://redis:6379/0')
 conn = Redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
-        worker.work(with_scheduler=True)
+    queues = [Queue(name, connection=conn) for name in listen]
+    worker = Worker(queues, connection=conn)
+    worker.work(with_scheduler=True)
+    print("Worker started, listening to queues:", listen)
